ad7e5f8b80c6d1f37e4d22963c3d2cdae08c8be6
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2025-09-03 23:48:29 -0400

    Add openfactory_timestamp

--- openfactory/assets/utils.py
+++ openfactory/assets/utils.py
@@ -5,6 +5,22 @@ from dataclasses import dataclass, field
 from typing import Union, Literal
 
 
+def openfactory_timestamp(timestamp: datetime) -> str:
+    """
+    Convert a datetime to OpenFactory timestamp format.
+
+    The format is ISO 8601 with milliseconds precision and a 'Z' to indicate UTC time,
+    e.g., '2025-05-04T12:34:56.789Z'.
+
+    Args:
+        timestamp (datetime.datetime): A datetime object (UTC recommended).
+
+    Returns:
+        str: Timestamp formatted in OpenFactory style.
+    """
+    return timestamp.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+
+
 def current_timestamp() -> str:
     """
     Returns the current timestamp in OpenFactory format.
@@ -15,7 +31,7 @@ def current_timestamp() -> str:
     Returns:
         str: The current UTC timestamp formatted in OpenFactory style.
     """
-    return datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+    return openfactory_timestamp(datetime.now(timezone.utc))
 
 
 @dataclass
